bb8f5823f563b9eb87f96c6510eaec78d666a4b3
Miguel Sandim <Miguel Sandim>
2017-04-07 17:24:27 +0100

    Loaded datasets for experiments

--- process_datasets.R
+++ process_datasets.R
@@ -1,6 +1,9 @@
 library(dplyr)
 library(foreign)
 
+## Glass
+dataset <- read.arff("datasets/literature/Glass/Glass_withoutdupl_norm.arff")
+
 ## KDDCUP99
 dataset <- read.arff("datasets/literature/KDDCup99/KDDCup99_withoutdupl_1ofn.arff")
 #dataset <- dataset %>% select(-outlier, -id)
@@ -10,3 +13,5 @@ write.csv(dataset, "datasets_processed/KDDCup99.csv", row.names = FALSE)
 dataset <- read.arff("datasets/literature/ALOI/ALOI_withoutdupl.arff")
 #dataset <- dataset %>% select(-outlier, -id)
 write.csv(dataset, "datasets_processed/ALOI.csv", row.names = FALSE)
+
+## 
